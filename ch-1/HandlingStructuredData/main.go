5374a609e662bef22d958dbc701f7771645ac6c2
ahmed mazhar <ahmedmazhar666@gmail.com>
2025-04-07 20:13:24 +0200

    modified:   ch-1/HandlingStructuredData/main.go

--- ch-1/HandlingStructuredData/main.go
+++ ch-1/HandlingStructuredData/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// --------------------- JSON MARSHAL/UNMARSHAL EXAMPLE ---------------------
+	// Create a Foo struct instance with some data
+	f := Foo{"Joe Junior", "Hello Shabado"}
+
+	// Marshal the Foo struct into JSON
+	b, _ := json.Marshal(f) // Converts struct to []byte JSON
+
+	// Print the JSON as a string
+	fmt.Println(string(b)) // Output: {"Bar":"Joe Junior","Baz":"Hello Shabado"}
+
+	// Unmarshal the JSON back into the struct (just to demonstrate)
+	json.Unmarshal(b, &f)
+
+	// --------------------- XML MARSHAL EXAMPLE WITH TAGS ---------------------
+	// Create a Boo struct instance with same values
+	x := Boo{"Joe Junior", "Hello Shabado"}
+
+	// Marshal the Boo struct into formatted XML
+	output, err := xml.MarshalIndent(x, "", "  ") // Nicely indented
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Print the resulting XML string
+	fmt.Println(string(output))
+}
+
+// Foo is a basic struct with two fields (used for JSON marshaling)
+type Foo struct {
+	Bar string
+	Baz string
+}
+
+// Boo is a struct where XML tags define how it should be marshaled to XML
+type Boo struct {
+	Bar string `xml:"id,attr"`      // Bar will become an XML attribute called "id"
+	Baz string `xml:"parent>child"` // Baz will be nested inside <parent><child>...</child></parent>
 }
